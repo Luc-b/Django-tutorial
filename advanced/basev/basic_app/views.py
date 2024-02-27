84c0776891c8cd7395d8d7825810f6007b4c899e
Luc-b <lucijanabubnjic@gmail.com>
2024-02-27 20:15:16 +0100

    'CRUD'

--- advanced/basev/basic_app/views.py
+++ advanced/basev/basic_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView,ListView, DetailView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import (View,TemplateView,ListView, 
+                                  DetailView,CreateView,
+                                  UpdateView,DeleteView) 
 from . import models
 
 class IndexView(TemplateView):
@@ -15,9 +18,18 @@ class SchoolDetailView(DetailView):
     model=models.School
     template_name='basic_app/school_detail.html'
 
+class SchoolCreateView(CreateView):
+    fields=('name', 'principal','location')
+    model=models.School
 
+class SchoolUpdateView(UpdateView):
+    fields=('name', 'principal')
+    model=models.School
 
-
+class SchoolDeleteView(DeleteView):
+    model=models.School
+    success_url=reverse_lazy('basic_app:list')
+    
 #     def get_context_data(self, **kwargs):
 #         context = super().get_context_data(**kwargs)
 #         context['injectme']='BASIC INJECTION'
